b17f835c81a56d72e7c3b04e6b8dd0f881e05fee
Zeztron <pateltharsh@gmail.com>
2021-05-30 11:57:37 -0400

    added main method

--- blockchain.py
+++ blockchain.py
@@ -14,9 +14,13 @@ class Blockchain:
     def __repr__(self):
         return f'Blockchain: {self.chain}'
 
+def main():
+    blockchain = Blockchain()
+    blockchain.add_block('one')
+    blockchain.add_block('two')
 
-blockchain = Blockchain()
-blockchain.add_block('one')
-blockchain.add_block('two')
+    print(blockchain)
+    print(f'blockchain.py __name__: {__name__}')
 
-print(blockchain)
+if __name__ == '__main__':
+    main()
