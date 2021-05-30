ce8c5853c675c0c914f1406f5f4539f871e61d43
Zeztron <pateltharsh@gmail.com>
2021-05-30 11:45:01 -0400

    added add_block method

--- blockchain.py
+++ blockchain.py
@@ -1,2 +1,20 @@
+class Block:
+    """
+    Block: a unit of storage.
+    Store transactions in a blockchain that supports a cryptocurrency.
+    """
+    def __init__(self, data):
+        self.data = data
+
+
 class Blockchain:
-  
+    """
+    Blockchain: a public ledger of transactions.
+    Implemented as a list of blocks - datasets of transactions
+    """
+    def __init__(self):
+        self.chain = []
+    
+    def add_block(self, data):
+        self.chain.append(Block(data))
+    
