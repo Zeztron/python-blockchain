e6b873bb40b361adcca7ea98759803f38ee78e89
Zeztron <pateltharsh@gmail.com>
2021-06-02 09:28:08 -0400

    updated crypto hash function

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,16 +1,22 @@
 import hashlib
 import json
 
-def crypto_hash(data):
+def crypto_hash(*args):
     """
-    Return a sha-256 hash of the given data.
+    Return a sha-256 hash of the given arguments.
     """
-    stringify_data = json.dumps(data)
+    stringify_args = map(lambda data: json.dumps(data), args)
 
-    return hashlib.sha256(stringify_data.encode('utf-8')).hexdigest()
+    print(f'stringify_args: {stringify_args}')
+
+    joined_data = ''.join(stringify_args)
+
+    print(f'joined_data: {joined_data}')
+
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
-    print(f"crypto_hash('foo'): {crypto_hash('foo')}")
+    print(f"crypto_hash('one', 2, [3]): {crypto_hash('one', 2, [3])}")
 
 if __name__ == '__main__':
     main()
