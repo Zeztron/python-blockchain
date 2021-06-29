fc26cb3f75a047e1a479362c6c88088057011437
Zeztron <pateltharsh@gmail.com>
2021-06-29 09:11:48 -0500

    block method returns hash

--- crypto_hash.py
+++ crypto_hash.py
@@ -5,18 +5,15 @@ def crypto_hash(*args):
     """
     Return a sha-256 hash of the given arguments.
     """
-    stringify_args = map(lambda data: json.dumps(data), args)
-
-    print(f'stringify_args: {stringify_args}')
+    stringify_args = sorted(map(lambda data: json.dumps(data), args))
 
     joined_data = ''.join(stringify_args)
-
-    print(f'joined_data: {joined_data}')
 
     return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
     print(f"crypto_hash('one', 2, [3]): {crypto_hash('one', 2, [3])}")
+    print(f"crypto_hash(2, 'one', [3]): {crypto_hash(2, 'one', [3])}")
 
 if __name__ == '__main__':
     main()
